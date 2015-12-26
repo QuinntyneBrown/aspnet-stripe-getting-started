56343ab4268c4369e8da2057727c20e68a38b614
Quinntyne Brown <quinntynebrown@gmail.com>
2015-12-26 14:34:40 -0500

    clean up stripe configuration

--- AspNet.Stripe.Web/App_Start/UnityConfig.cs
+++ AspNet.Stripe.Web/App_Start/UnityConfig.cs
@@ -3,6 +3,8 @@ using Microsoft.Practices.Unity;
 using Unity.Mvc5;
 using AspNet.Stripe.Web.Services.Contracts;
 using AspNet.Stripe.Web.Services;
+using AspNet.Stripe.Web.Config.Contracts;
+using AspNet.Stripe.Web.Config;
 
 namespace AspNet.Stripe.Web
 {
@@ -12,6 +14,7 @@ namespace AspNet.Stripe.Web
         {
 			var container = new UnityContainer();
             container.RegisterType<IProductService, ProductService>();
+            container.RegisterType<IConfigProvider, ConfigProvider>();
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
--- AspNet.Stripe.Web/Services/ProductService.cs
+++ AspNet.Stripe.Web/Services/ProductService.cs
@@ -1,16 +1,23 @@
-﻿using AspNet.Stripe.Web.Models;
+﻿using AspNet.Stripe.Web.Config.Contracts;
+using AspNet.Stripe.Web.Models;
 using AspNet.Stripe.Web.Services.Contracts;
-using System.Configuration;
 
 namespace AspNet.Stripe.Web.Services
 {
     public class ProductService: IProductService
     {
+        public ProductService(IConfigProvider configProvider)
+        {
+            this.stripeConfiguration = configProvider.Get<IStripeConfiguration>();
+        }
+
         public ProductViewModel Get()
         {
             var viewModel = new ProductViewModel();
-            viewModel.StripePublishableKey = ConfigurationManager.AppSettings["stripePublishableKey"];
+            viewModel.StripePublishableKey = stripeConfiguration.StripePublishableKey;
             return viewModel;
         }
+
+        protected readonly IStripeConfiguration stripeConfiguration;
     }
 }
